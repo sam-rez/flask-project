c7f2ec7c515639f361637aab4447d7dbcc3d4caa
Sam Rezaie <srezaie1226@gmail.com>
2022-11-01 17:16:48 -0700

    Setup templates, sign up page, and flashes

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,44 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>login</p>"
+    data = request.form
+    print(data)
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # user = User.query.filter_by(email=email).first()
+        # if user:
+        #     flash('Email already exists.', category='error')
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            pass
+            # new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+            #     password1, method='sha256'))
+            # db.session.add(new_user)
+            # db.session.commit()
+            # login_user(new_user, remember=True)
+            # flash('Account created!', category='success')
+            # return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html")
