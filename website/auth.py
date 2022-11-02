f02eb40b948090701c660caf04c6fd3d9ac4434e
Sam Rezaie <srezaie1226@gmail.com>
2022-11-01 20:07:39 -0700

    Login and Logout user

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -13,19 +16,21 @@ def login():
         if user:
             if user.password == password:
                 flash('Logged in successfully!', category='success')
-                #login_user(user, remember=True)
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -51,8 +56,8 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=password1)
             db.session.add(new_user)
             db.session.commit()
-            #login_user(new_user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
